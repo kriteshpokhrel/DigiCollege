27fe885a3bbd2c3cef92f12210c81984051f79b8
Kritesh Pokhrel <kriteshpokharel100@gmail.com>
2020-11-25 15:56:03 +0545

    2nd update assignment added

--- studentHome.py
+++ studentHome.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 from teacherSettings import *
 from viewClassroom import *
@@ -6,9 +5,10 @@ from studentSettings import *
 global studentColour
 studentColour = "#f2820d"
 class sHome:
-    def __init__(self, sLogName):
+    def __init__(self, sLogName,sec):
         global sHome1
         self.logName = sLogName
+        self.sec = sec
         sHome1= Toplevel()
         sHome1.geometry("640x360")
         sHome1.title("Students Home Page")
@@ -73,6 +73,51 @@ class sHome:
                     e.insert(END, books[j])
                 i += 1
             listIsBook.mainloop()
+
+    def asmtStdnt(self):
+        global asmtStdnt1
+        asmtStdnt1 = Toplevel()
+        asmtStdnt1.geometry('930x300')
+        asmtStdnt1.title("Assignments")
+
+        # top Frame
+        topFrame = Frame(asmtStdnt1)
+        topFrame.pack(side=TOP, fill=X)
+        # header
+        self.tHeader = Label(topFrame, text="Assignments", bg=studentColour, fg='white',
+                             font=("Sans serif", 16, "bold"), pady=6)  # text add
+        self.tHeader.pack(fill=X)  # pack is a must
+
+        # btm frame
+        btmFrame = Frame(asmtStdnt1)
+        btmFrame.pack(fill=X)
+        btmFrame.place(y=50)
+        conn = connection.cursor()
+        sql = 'SELECT * FROM assignment WHERE Section = "{}"'.format(
+            self.sec)
+        res = conn.execute(sql)
+        asmtLst = conn.fetchall()
+
+        details = ["Subject", "Name", "Section", "Link", "Due_Date", "Remarks", "Teacher"]
+        i = 0
+        width1 = ["15", "25", "10", "35", "15", "30", "20"]
+        # creating headings
+        for j in range(len(details)):
+            e = Entry(btmFrame, width=width1[j], bg=studentColour)
+            e.grid(row=i, column=j)
+            e.insert(END, details[j])
+        i += 1
+        # inserting datasframe1,pc
+        for asmt in asmtLst:
+            for j in range(len(asmt)):
+                e = Entry(btmFrame, width=width1[j], bg='white')
+                e.grid(row=i, column=j)
+                e.insert(END, asmt[j])
+            i += 1
+
+        asmtStdnt1.mainloop()
+
+
     def gui_1(self,sLogName):
 
         #header
@@ -87,11 +132,23 @@ class sHome:
         self.bkIssuedBtn.pack()
         self.bkIssuedBtn.place(x= '62',y= '200')
 
+        # assignment button
+        self.asmtBtnIcon = PhotoImage(file = "button_assignmentStudent.png")
+        self.asmtBtn = Button(sHome1, image = self.asmtBtnIcon, borderwidth = 0,command = self.asmtStdnt)
+        self.asmtBtn.pack()
+        self.asmtBtn.place(x= '240',y= '200')
+
         # book Issued photo
         self.yClrPhoto= PhotoImage(file = "icon_booksIssuedStudent.png")
         yClrPhotoLbl = Label(sHome1, image= self.yClrPhoto)
         yClrPhotoLbl.pack()
         yClrPhotoLbl.place(x= "70", y= "70")
+
+        # assignment photo
+        self.asmtPhoto= PhotoImage(file = "icon_asssignmentStudent.png")
+        asmtPhotoLbl = Label(sHome1, image= self.asmtPhoto)
+        asmtPhotoLbl.pack()
+        asmtPhotoLbl.place(x= "250", y= "70")
 
         # settings button
         self.settingsBtnIcon = PhotoImage(file ="button_settingsStudent.png")
@@ -100,9 +157,6 @@ class sHome:
         self.settingsBtn.place(x= '585',y= '308')
 
 
-
-
-
 if __name__ == "__main__":
-    t1= sHome("")
+    t1= sHome("","")
     t1
